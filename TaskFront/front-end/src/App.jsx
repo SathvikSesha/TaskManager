dc092627ba04fb3149834db78ab4377317046964
SathvikSesha <lakshmisarmatk@gmail.com>
2026-01-12 14:58:26 +0530

    Added Tailwind and Protected Routing

--- TaskFront/front-end/src/App.jsx
+++ TaskFront/front-end/src/App.jsx
@@ -2,13 +2,16 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./Pages/DashBoard";
 import ProtectedRoutes from "./Routes/ProtectedRoutes";
 import Login from "./Pages/Login";
+import LandingPage from "./Pages/LandingPage";
+import SignUp from "./Pages/SignUp";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/" element={<LandingPage />} />
 
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
 
       <Route
         path="/dashboard"
