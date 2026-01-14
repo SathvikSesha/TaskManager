a196c16470848f3251c16ebdabd87f992ef7146e
SathvikSesha <lakshmisarmatk@gmail.com>
2026-01-14 19:50:59 +0530

    Added the backend link

--- TaskFront/front-end/src/api/axios.js
+++ TaskFront/front-end/src/api/axios.js
@@ -1,16 +1,14 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://taskmanager-production-0f8a.up.railway.app/api",
 });
 
-// Attach token automatically
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-
   return config;
 });
 
