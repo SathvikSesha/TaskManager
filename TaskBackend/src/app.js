89dfdb55217a743b47a8cfc10f822c3177b80eac
SathvikSesha <lakshmisarmatk@gmail.com>
2026-01-15 11:48:34 +0530

    Added the frontend deployed link to server cors

--- TaskBackend/src/app.js
+++ TaskBackend/src/app.js
@@ -1,3 +1,22 @@
+// import express from "express";
+// import cors from "cors";
+// import authRoutes from "./routes/auth.routes.js";
+// import taskRoutes from "./routes/task.routes.js";
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.use("/api/auth", authRoutes);
+
+// app.get("/", (req, res) => {
+//   res.json({ message: "Backend is running 🚀" });
+// });
+
+// app.use("/api/tasks", taskRoutes);
+
+// export default app;
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
@@ -5,15 +24,20 @@ import taskRoutes from "./routes/task.routes.js";
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://taskly-topaz-sigma.vercel.app"],
+    credentials: true,
+  })
+);
 
-app.use("/api/auth", authRoutes);
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ message: "Backend is running 🚀" });
 });
 
+app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
 export default app;
