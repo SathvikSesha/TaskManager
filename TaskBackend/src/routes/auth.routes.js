8b46b66f373e9975092da51e613e518276ce122c
SathvikSesha <lakshmisarmatk@gmail.com>
2026-04-07 18:01:32 +0530

    Added new routes

--- TaskBackend/src/routes/auth.routes.js
+++ TaskBackend/src/routes/auth.routes.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { signup, login } from "../controllers/auth.controller.js";
+import {
+  signup,
+  login,
+  getGoogleAuthUrl,
+  googleAuthCallback,
+} from "../controllers/auth.controller.js";
 
 const router = express.Router();
-
 router.post("/signup", signup);
 router.post("/login", login);
+router.get("/google/url", getGoogleAuthUrl);
+router.get("/google/callback", googleAuthCallback);
 
 export default router;
