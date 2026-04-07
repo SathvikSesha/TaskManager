4e4d9c9b078a341a03c64528e8b400ed669125e7
SathvikSesha <lakshmisarmatk@gmail.com>
2026-04-07 17:43:09 +0530

    Changed the user model according to OAuth

--- TaskBackend/src/models/user.model.js
+++ TaskBackend/src/models/user.model.js
@@ -14,19 +14,30 @@ const userSchema = new mongoose.Schema(
     },
     password: {
       type: String,
-      required: true,
+    },
+    authProvider: {
+      type: String,
+      enum: ["local", "google"],
+      default: "local",
+    },
+    googleId: {
+      type: String,
+      unique: true,
+      sparse: true,
     },
   },
   { timestamps: true },
 );
 
 userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+  if (!this.isModified("password") || !this.password) return next();
 
   this.password = await bcrypt.hash(this.password, 10);
 });
 
 userSchema.methods.comparePassword = async function (enteredPassword) {
+  if (!this.password) return false;
+
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
