519a750890348504bf9d35bf31b46589b3c01c46
SathvikSesha <lakshmisarmatk@gmail.com>
2026-04-08 09:21:54 +0530

    Used callback to stop infinite loop

--- TaskFront/front-end/src/api/axios.js
+++ TaskFront/front-end/src/api/axios.js
@@ -12,4 +12,19 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      console.warn("Session expired. Automatically logging out...");
+      localStorage.removeItem("user");
+      localStorage.removeItem("token");
+      window.location.href = "/login?session_expired=true";
+    }
+    return Promise.reject(error);
+  },
+);
+
 export default api;
