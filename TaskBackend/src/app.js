e9928d3d1779b9feb409917d3491255338f0a45e
SathvikSesha <lakshmisarmatk@gmail.com>
2026-04-20 00:07:58 +0530

    Added correct app

--- TaskBackend/src/app.js
+++ TaskBackend/src/app.js
@@ -7,23 +7,13 @@ import taskRoutes from "./routes/task.routes.js";
 import paymentRoutes from "./routes/payment.routes.js";
 import { errorHandler } from "./middleware/error.middleware.js";
 
-const allowedOrigins = [
-  "http://localhost:5173", 
-  "https://taskly-topaz-sigma.vercel.app" 
-];
+const app = express();
 
+// 2. NOW apply CORS
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
+    origin: ["http://localhost:5173", "https://taskly-topaz-sigma.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   }),
 );
